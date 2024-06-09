754dc3b33703ff15cf9d3a6e401f1cc688d885b8
eyelessA <olegswift04@gmail.com>
2024-06-09 20:42:07 +0400

    feet: new router

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilmController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/films', [FilmController::class, 'index']);
