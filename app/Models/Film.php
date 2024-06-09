9e93954969f4a54a44568e94d76dbb9a40069109
eyelessA <olegswift04@gmail.com>
2024-06-09 20:41:57 +0400

    feet: new method

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -12,4 +12,9 @@ class Film extends Model
     use SoftDeletes;
 
     protected $guarded = false;
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'film_genres', 'film_id', 'genre_id');
+    }
 }
